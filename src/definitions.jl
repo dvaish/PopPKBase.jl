7bb90fe7b54a271c521b45792d8e4c44b463f091
Dhruv Vaish <dvaish@berkeley.edu>
2022-05-15 22:49:16 -0700

    redefining structs

--- src/definitions.jl
+++ src/definitions.jl
@@ -1,60 +1,72 @@
 using DataFrames
+using Random
 
 struct Individual
     id # ::Any
-    dose # ::Union{AbstractVector, Number, Missing}
-    dosing # ::Vector
+    amt
+    dosing
     observations # ::Vector
     covariates
-    function Individual(id, dose, dosing, observations; covariates...)
-        return new(id, dose, dosing, observations, covariates)
+    function Individual(;id = rand(UInt16), amt, dosing, observations, covariates...)
+        return new(id, amt, dosing, observations, covariates)
     end
 end
 
 struct Cohort
-    individuals
+    population
     length
-    function Cohort(individuals)
-        return new(individuals, length(individuals))
-    end
 end
 
-struct Run
-    id::Symbol
-    dose::Symbol
-    obs::Symbol
-    covariates::Tuple{Union{Symbol, String}}
-    data::DataFrame
+function Cohort(population)
+    return new(population, length(population))
 end
 
-@inline function getter(run::Run, field::Union{Symbol, String})
-    return run.data[getfield(run, field)]
+function Cohort(individuals, f)
+    population = map(f, individuals)
+    return Cohort(population)
 end
 
-function id(run::Run)
-    return getter(run, :id)
+function cohort!(individuals, f)
+    population = map!(f, individuals)
+    return Cohort(population)
 end
 
-function dose(run::Run)
-    return getter(run, :dose)
+# struct Run
+#     id::Symbol
+#     amt::Symbol
+#     obs::Symbol
+#     covariates::Tuple{Union{Symbol, String}}
+#     data::DataFrame
+# end
+
+# @inline function getter(run::Run, field::Union{Symbol, String})
+#     return run.data[getfield(run, field)]
+# end
+
+# @inline function id(run::Run)
+#     return getter(run, :id)
+# end
+
+# @inline function amt(run::Run)
+#     return getter(run, :amt)
+# end
+
+# @inline function obs(run::Run)
+#     return getter(run, :obs)
+# end
+
+function DataFrames.DataFrame(cohort::Cohort)
+    individuals = map(ind -> append!(df, dataframe(ind)), cohort.individuals)
+    dataframe = vcat(individuals...)
+    return dataframe
 end
 
-function obs(run::Run)
-    return getter(run, :obs)
-end
 
-function dataframe(cohort::Cohort)
-    df = DataFrame()
-    map(ind -> append!(df, dataframe(ind)), cohort.individuals)
-    return df
-end
-
-
-function dataframe(individual::Individual)
+function DataFrames.DataFrame(individual::Individual)
     time = vcat(individual.dosing, individual.observations)
     entries = length(time)
     amt = zeros(entries)
-    amt[1:length(individual.dosing)] .= coalesce(individual.dose, 0)
+    amt[1:length(individual.dosing)] .= coalesce(individual.amt, 0)
     indices = sortperm(time)
     covs = map((key, value) -> key => fill(value, entries), keys(individual.covariates), values(individual.covariates))
     id = fill(individual.id, entries)
@@ -64,7 +76,7 @@ function dataframe(individual::Individual)
     return DataFrame(
         :ID => id, 
         :TIME => time,
-        :EVID => Vector{Int64}(amt .> 0), # What if dose is zero for EVID == 1
+        :EVID => Vector{Int64}(amt .> 0), # What ioamtis zero for EVID == 1
         :AMT => amt,
         :DV => dv,
         covs...
